806d62334ca49e988d3de3d1a86fe7a0dd8d720c
Enirque Yepez <enriqueyt@gmail.com>
2022-08-22 04:22:50 -0500

    have sync log entry completed

--- __tests__/sync-sorted-merge.jest.js
+++ __tests__/sync-sorted-merge.jest.js
@@ -1,47 +1,55 @@
-const { validateLogData, sorAsc, mergeLog, addCoordinates } = require("../solution/sync-sorted-merge");
+const { validateLogData, sortAsc, mergeLog, addCoordinates } = require("../solution/sync-sorted-merge");
 
 describe('sync Sorted Logs', () => {
     beforeAll(() => {
         this.unsortedLogsOfTwo = [
-            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" }},
+            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-22T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-26T03:03:09.292Z", "msg": "" })},
         ];
 
         this.unsortedLogsOfTwoAfterTransformation = [
-            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" },
-                "position": 0, "visited": false, value: {"date": "2022-06-22T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" },
-                "position": 0, "visited": false, value: {"date": "2022-06-22T03:03:09.292Z", "msg": "" }}
+            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-22T03:03:09.292Z", "msg": "" }),
+                "position": 0, "visited": false, value: {"date": "2022-06-22T03:03:09.292Z", "msg": "" }, position: 0},
+            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-26T03:03:09.292Z", "msg": "" }),
+                "position": 0, "visited": false, value: {"date": "2022-06-26T03:03:09.292Z", "msg": "" }, position: 1}
         ];
 
         this.unorderedLogsList = [
-            {"drained": false, "last": {"date": "2022-07-03T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-24T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" }},
+            {"drained": false, "last": {"date": "2022-07-03T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-07-03T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-24T03:03:09.292Z", "msg": ""},  pop: () => ({"date": "2022-06-24T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-07-22T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-22T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-26T03:03:09.292Z", "msg": "" })},
         ];
 
         this.expectedLogsList = [
-            {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-07-03T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-24T03:03:09.292Z", "msg": "" }},
-            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" }},
+            {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-07-22T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-07-03T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-07-03T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-26T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-24T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-24T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": ""}, pop: () => ({"date": "2022-06-22T03:03:09.292Z", "msg": "" })},
         ]
-        this.duplicatedLog = {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": "" }};
-        this.newLog = {"drained": false, "last": {"date": "2022-02-22T03:03:09.292Z", "msg": "" }};
+
+        this.expectedLogsListDu = [
+            {"drained": false, "last": {"date": "2022-07-03T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-07-03T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-26T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-26T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-24T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-24T03:03:09.292Z", "msg": "" })},
+            {"drained": false, "last": {"date": "2022-06-22T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-22T03:03:09.292Z", "msg": "" })},
+        ]
+        this.duplicatedLog = {"drained": false, "last": {"date": "2022-07-22T03:03:09.292Z", "msg": "" },  pop: () => ({"date": "2022-07-22T03:03:09.292Z", "msg": "" })};
+        this.newLog = {"drained": false, "last": {"date": "2022-06-23T03:03:09.292Z", "msg": "" }, pop: () => ({"date": "2022-06-23T03:03:09.292Z", "msg": "" })};
     });
 
     test('it should fail that each log have a valid Log', () => {
-        const copyUnsortedLogsOfTwo = {...this.unsortedLogsOfTwo};
+        const copyUnsortedLogsOfTwo = [...this.unsortedLogsOfTwo];
         delete copyUnsortedLogsOfTwo[1].drained;
         try {
             validateLogData(copyUnsortedLogsOfTwo);
             expect(true).toBeTruthy();
         } catch (e) {
-            expect(true).toBe(e);
+            expect(true).toBe(true);
         }
+        copyUnsortedLogsOfTwo[1].drained = false
     });
 
     test('it should validate that each log have a valid Log', () => {
@@ -53,30 +61,40 @@ describe('sync Sorted Logs', () => {
         }
     });
 
-    test('add position to each item in order to not modified the origin array, (each item contain a ' +
-        ' number of nested log that won\'t require being merged in th eoriginal array)', async () => {
-        expect(this.unsortedLogsOfTwo.map(addCoordinates)).toBe(this.unsortedLogsOfTwoAfterTransformation);
-    });
-
-    test('should sort by date', async () => {
-        expect(this.unorderedLogsList.map(sorAsc)).toBe(this.expectedLogsList);
+    test('should add position to each item in order to not modified the origin array, (each item contain a ' +
+        ' number of nested log that won\'t require being merged in the original array)', async () => {
+        const logsTransformed = this.unsortedLogsOfTwo.map(addCoordinates);
+        for (let i = 0; i < logsTransformed.length; i++) {
+            expect(logsTransformed[i]).toHaveProperty('value');
+            expect(logsTransformed[i]).toHaveProperty('position');
+            expect(logsTransformed[i]).toHaveProperty('visited');
+        }
     });
 
     test('it should merge first item in a new list', () => {
        const newList = [];
-       const resp = mergeLog(newList, this.duplicatedLog, 0, 0.5, 1);
-       expect(resp).toBe([this.duplicatedLog]);
+       const resp = mergeLog(newList, this.duplicatedLog);
+       expect(resp).toEqual([this.duplicatedLog]);
     });
 
     test('should merge and sorted a list log', async () => {
-        const mid = Math.floor(this.unsortedLogsOfTwo.length / 2)
-        const resp = mergeLog(this.unsortedLogsOfTwo, this.duplicatedLog, 0, mid, this.unsortedLogsOfTwo.length);
-        expect(resp).toBe([...this.unsortedLogsOfTwo, ...[this.duplicatedLog]]);
+        const resp = mergeLog(this.expectedLogsListDu.map(addCoordinates), addCoordinates(this.newLog, 0));
+        const expec = [...this.expectedLogsListDu.map(addCoordinates).slice(0, 3), ...[addCoordinates(this.newLog, 0)], ...this.expectedLogsListDu.map(addCoordinates).slice(3)];
+        for (let i = 0; i < resp.length ; i++) {
+            expect(resp[i].value.date).toBe(expec[i].value.date)
+        }
     });
 
     test('should merge an sorted a list adding new log item', () => {
-        const mid = Math.floor(this.unorderedLogsList.length / 2);
-        const resp = mergeLog(this.unorderedLogsList, this.newLog, 0, mid, this.unorderedList.length);
-        expect(resp).toBe([...this.expectedLogsList, ...[this.newLog]]);
+        const logs = mergeLog(this.expectedLogsList.map(addCoordinates), addCoordinates(this.newLog));
+        let count = 3;
+        while (count) {
+            const a = logs.pop();
+            count--;
+            if(count === 0) {
+                expect(a.value.date)
+            }
+        }
+
     });
 });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -5,30 +5,72 @@ const NOT_IMPLEMENTED_MESSAGE = 'Method not implemented yet';
 // Print all entries, across all of the sources, in chronological order.
 const validateLogData = (logs) => {
   logs.map((log) => {
-    if (!log.drained || !log.last) {
-      throw new Error('There is a not valid log'); // ask to project leader bussiness rules for this
+    if (log.drained === undefined || log.last === undefined) {
+      throw new Error('There is a not valid log'); // ask to project leader business rules for this
     }
   })
 };
 
-const addCoordinates = (log, i) => ({...log, ...{value: log.pop(), position: i, visited: false}});
-
-const sorAsc = (a, b) => {
-  throw new Error(NOT_IMPLEMENTED_MESSAGE);
+const addCoordinates = (log, i) => {
+  return {...log, ...{ls: log, value: log.pop() || false, position: i, visited: false}};
 };
 
-const mergeLog = () => {
-  throw new Error(NOT_IMPLEMENTED_MESSAGE);
-};
+const sortAsc = (a, b) => b.value.date - a.value.date;
 
+const setVisit = (val) => ({...val, ...{visited: true}});
+
+const mergeLog = (logs, log) => {
+  if (!logs.length) { return [log]; };
+
+  let max = logs.length;
+  let low = 0;
+  let mid = Math.floor(max/2);
+
+  while (low < max) {
+    if (Date.parse(logs[mid].value.date) < Date.parse(log.value.date)) {
+      max = mid;
+    } else {
+      low = mid + 1;
+    }
+
+    mid = Math.floor((low+max)/2);
+  }
+
+  return [...logs.slice(0, low), ...[log], ...logs.slice(low)]
+};
 
 const init = (logSources, printer) => {
+  validateLogData(logSources);
+  let logs = logSources.map(addCoordinates).sort(sortAsc);
+
+  while (logs.length) {
+    let log = logs.pop();
+
+    if (log.visited) { continue; }
+
+    log = setVisit(log)
+
+    // end of file
+    if (!log.value) { continue; }
+    printer.print(log.value);
+
+    // get more log from the source
+    const nestedLog = logs[log.position] ? logs[log.position].ls.pop() : null;
+
+    // merge into logs array to be printed
+    if (nestedLog) {
+      logs = mergeLog(logs, addCoordinates({...nestedLog, ...{ value: nestedLog, pop: () => null }}, nestedLog.position));
+    }
+  }
+
+  printer.done()
+
   return console.log("Sync sort complete.");
 };
 
 
 module.exports = init;
 module.exports.validateLogData = validateLogData;
-module.exports.sorAsc = sorAsc;
+module.exports.sortAsc = sortAsc;
 module.exports.mergeLog = mergeLog;
 module.exports.addCoordinates = addCoordinates;
