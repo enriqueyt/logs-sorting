124bd29aee8213c28e73f12335e2f23f1f1e6552
Enirque Yepez <enriqueyt@gmail.com>
2022-08-22 00:06:09 -0500

    validation/test definitions for adding coordiantes and a valid log object

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,11 +3,17 @@
 const NOT_IMPLEMENTED_MESSAGE = 'Method not implemented yet';
 
 // Print all entries, across all of the sources, in chronological order.
-const validateLogData = () => {
-  throw new Error(NOT_IMPLEMENTED_MESSAGE);
+const validateLogData = (logs) => {
+  logs.map((log) => {
+    if (!log.drained || !log.last) {
+      throw new Error('There is a not valid log'); // ask to project leader bussiness rules for this
+    }
+  })
 };
 
-const sorAsc = () => {
+const addCoordinates = (log, i) => ({...log, ...{value: log.pop(), position: i, visited: false}});
+
+const sorAsc = (a, b) => {
   throw new Error(NOT_IMPLEMENTED_MESSAGE);
 };
 
@@ -15,9 +21,14 @@ const mergeLog = () => {
   throw new Error(NOT_IMPLEMENTED_MESSAGE);
 };
 
+
 const init = (logSources, printer) => {
   return console.log("Sync sort complete.");
 };
 
 
 module.exports = init;
+module.exports.validateLogData = validateLogData;
+module.exports.sorAsc = sorAsc;
+module.exports.mergeLog = mergeLog;
+module.exports.addCoordinates = addCoordinates;
